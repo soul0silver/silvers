b10d32022b0f4a37d0002a9496e360201f725838
soul0silver <122255949+soul0silver@users.noreply.github.com>
2023-04-18 22:31:54 +0700

    dat

--- ss8/app.js
+++ ss8/app.js
@@ -1,5 +1,17 @@
-function say(mess){
-         return mess
-}
+
+
+function functionUserDefine(){
+                    let s=['co 01 chu cai viet hoa','co it nhat 1 so','co it nhat 1 ky tu dac BiquadFilterNode'];
+                    let count=[0,0,0];
+                    let text="";
+                    text=document.getElementById('mess').value;
+                    for (let i=0;i<text.length;i++) 
+                    if (text[i]>='A'&&text[i]<='Z') count[0]++;
+                    else if(text[i]>='0'&&text[i]<='9') count[1]++;
+                    else if(text[i]>='!'&&text[i]<='/') count[2]++;       
+                    let show=s[0]+'<br>'+s[1]+'<br>'+s[2];
+                    for(let i=0;i<=2;i++) if (count[i]>=1) show-=s[i] ;
+                    document.getElementById('de').innerHTML=show
+};
 let res=say('hello');
 console.log('res',res);
